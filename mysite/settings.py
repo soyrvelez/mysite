2b4ba0d9802f5fc7bf380a35c07536fc2c16a197
Rome Bell <romebell5@gmail.com>
2024-01-31 22:24:30 -0800

    feat: setup db, add question and choice model

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -76,11 +77,16 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+MONGO_URI = os.getenv('MONGO_URI')
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'mysite',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': MONGO_URI
+            }  
+        }
 }
 
 
@@ -102,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Los_Angeles"
 
 USE_I18N = True
 
