406cac5e4526aa4ff815c921ea7666ab65ba7b22
Rodrigo Vélez <Velez59@gmail.com>
2024-02-10 19:09:47 -0800

    feat: working email connection

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,6 +87,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 MONGO_URI = os.getenv('MONGO_URI')
 PASSWORD = os.getenv('PASSWORD')
+EMAIL = os.getenv('EMAIL')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True),
 }
@@ -132,4 +134,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = EMAIL  
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+EMAIL_USE_TLS = True
